c618956f740574f21f73c756e6b01f67adf8f46b
Albert <jdfreerun@gmail.com>
2022-08-25 19:59:32 +0300

    Initial Commit

---
+++ NewsAppForOtus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAppForOtus
+//
+//  Created by Albert Fayzullin on 25.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
